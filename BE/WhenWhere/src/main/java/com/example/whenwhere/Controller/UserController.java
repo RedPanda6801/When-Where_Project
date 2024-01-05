010da27d0db54abf2e56589402c0bea4d91336b6
Redpanda6801 <redpanda6801@gmail.com>
2024-01-05 15:45:44 +0900

    머지 체크

--- BE/WhenWhere/src/main/java/com/example/whenwhere/Controller/UserController.java
+++ BE/WhenWhere/src/main/java/com/example/whenwhere/Controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @RequestMapping("/api/user")
 public class UserController {
